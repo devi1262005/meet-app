92d4f1cbfab29416980986450a94796bb49f0053
Magesh1007 <magemagesh1007@gmail.com>
2024-08-30 11:50:36 +0530

    Update settings.dart

--- meet/lib/page/settings.dart
+++ meet/lib/page/settings.dart
@@ -1,16 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+// ignore: depend_on_referenced_packages
 import 'package:google_fonts/google_fonts.dart';
 
-class Settings extends StatefulWidget {
-  const Settings({super.key});
+class Setting extends StatefulWidget {
+  const Setting({super.key});
 
   @override
-  State<Settings> createState() => _SettingsState();
+  State<Setting> createState() => _CreateState();
 }
 
-class _SettingsState extends State<Settings> {
-  bool isToggled = false;
+class _CreateState extends State<Setting> {
+  bool _notificationsEnabled = true;
+  String _selectedTheme = 'Light';
+  bool _locationEnabled = false;
+  double _micVolume = 0.5; // Volume level for Mic from 0.0 to 1.0
+  double _speakerVolume = 0.5; // Volume level for Speaker from 0.0 to 1.0
+
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      statusBarIconBrightness: Brightness.dark,
+      statusBarBrightness: Brightness.light,
+      statusBarColor: Colors.transparent,
+    ));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,92 +35,173 @@ class _SettingsState extends State<Settings> {
       appBar: AppBar(
         elevation: 4,
         backgroundColor: Colors.white,
-        shadowColor: Colors.black.withOpacity(0.3),
-         leading: IconButton(
+        shadowColor: Colors.black.withOpacity(0.1),
+        title: Text(
+          'Setting',
+          style: GoogleFonts.poppins(
+            fontSize: 22,
+            color: Colors.purple.shade800,
+            fontWeight: FontWeight.normal,
+          ),
+        ),
+        leading: IconButton(
           icon: FaIcon(
-            FontAwesomeIcons.angleLeft,
+            FontAwesomeIcons.arrowLeft,
             color: Colors.purple.shade800,
           ),
           onPressed: () {
             Navigator.pop(context);
           },
         ),
-         
-      ),
-      
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Settings',
-              style: GoogleFonts.poppins(
-                fontSize: 30,
-                color: Colors.purple.shade800,
-                fontWeight: FontWeight.normal,
-              ),
-            ),
-            SizedBox(height: 50),
-            Row(
-              children: [
-                Icon(Icons.video_call),
-                SizedBox(width: 10),
-                Text(
-                  'Video',
-                  style: GoogleFonts.poppins(
-                    fontSize: 20,
-                    color: Colors.black,
-                    fontWeight: FontWeight.normal,
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(height: 30),
-            Row(
-              children: [
-                Icon(Icons.audiotrack),
-                SizedBox(width: 10),
-                Text(
-                  'Audio',
-                  style: GoogleFonts.poppins(
-                    fontSize: 20,
-                    color: Colors.black,
-                    fontWeight: FontWeight.normal,
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(height: 30),
-            Row(
-              children: [
-                Icon(Icons.brightness_4_outlined),
-                SizedBox(width: 10),
-                Text(
-                  'Theme',
-                  style: GoogleFonts.poppins(
-                    fontSize: 20,
-                    color: Colors.black,
-                    fontWeight: FontWeight.normal,
-                  ),
-                ),
-                Spacer(),
-                IconButton(
-                  onPressed: () {
-                    setState(() {
-                      isToggled = !isToggled;
-                    });
-                  },
-                  icon: FaIcon(
-                    isToggled ? FontAwesomeIcons.toggleOn : FontAwesomeIcons.toggleOff,
-                    color: Colors.black,
-                  ),
-                ),
-              ],
-            ),
-          ],
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(1.0),
+          child: Container(
+            color: Colors.black.withOpacity(0.2),
+            height: 1.0,
+          ),
         ),
       ),
+      body: ListView(
+        children: <Widget>[
+          SwitchListTile(
+            title: const Text('Enable Notifications'),
+            value: _notificationsEnabled,
+            onChanged: (bool value) {
+              setState(() {
+                _notificationsEnabled = value;
+              });
+            },
+            secondary: const Icon(Icons.notifications),
+          ),
+          ListTile(
+            title: const Text('Theme'),
+            subtitle: Text(_selectedTheme),
+            leading: const Icon(Icons.color_lens),
+            onTap: () {
+              _selectTheme(context);
+            },
+          ),
+          SwitchListTile(
+            title: const Text('Enable Location'),
+            value: _locationEnabled,
+            onChanged: (bool value) {
+              setState(() {
+                _locationEnabled = value;
+              });
+            },
+            secondary: const Icon(Icons.location_on),
+          ),
+          ListTile(
+            title: Row(
+              children: const [
+                Icon(Icons.volume_up),
+                SizedBox(width: 8), // Space between icon and text
+                Text('Audio'),
+              ],
+            ),
+            onTap: () {
+              // Navigate to Audio settings page
+            },
+            subtitle: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 30.0), // Left padding for "Mic"
+                  child: const Text('Mic'),
+                ),
+                Slider(
+                  value: _micVolume,
+                  onChanged: (double value) {
+                    setState(() {
+                      _micVolume = value;
+                    });
+                  },
+                  min: 0.0,
+                  max: 1.0,
+                  divisions: 10,
+                  label: '${(_micVolume * 100).toInt()}%',
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 30.0), // Left padding for "Speaker"
+                  child: const Text('Speaker'),
+                ),
+                Slider(
+                  value: _speakerVolume,
+                  onChanged: (double val) {
+                    setState(() {
+                      _speakerVolume = val;
+                    });
+                  },
+                  min: 0.0,
+                  max: 1.0,
+                  divisions: 10,
+                  label: '${(_speakerVolume * 100).toInt()}%',
+                ),
+              ],
+            ),
+          ),
+          ListTile(
+            title: const Text('Account'),
+            leading: const Icon(Icons.account_circle),
+            onTap: () {
+              // Navigate to Account settings page
+            },
+          ),
+          ListTile(
+            title: const Text('Privacy'),
+            leading: const Icon(Icons.lock),
+            onTap: () {
+              // Navigate to Privacy settings page
+            },
+          ),
+          ListTile(
+            title: const Text('About'),
+            leading: const Icon(Icons.info),
+            onTap: () {
+              // Navigate to About page
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _selectTheme(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Select Theme'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                RadioListTile<String>(
+                  title: const Text('Light'),
+                  value: 'Light',
+                  groupValue: _selectedTheme,
+                  onChanged: (String? value) {
+                    setState(() {
+                      _selectedTheme = value!;
+                    });
+                    Navigator.of(context).pop();
+                  },
+                ),
+                RadioListTile<String>(
+                  title: const Text('Dark'),
+                  value: 'Dark',
+                  groupValue: _selectedTheme,
+                  onChanged: (String? value) {
+                    setState(() {
+                      _selectedTheme = value!;
+                    });
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
