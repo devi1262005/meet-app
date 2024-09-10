a0d1748eeaee921262f329850276640049cc5588
Devisree <143163502+devi1262005@users.noreply.github.com>
2024-09-10 14:09:23 +0530

    Update main.dart

--- meet/lib/main.dart
+++ meet/lib/main.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'page/Splash.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'page/Splash.dart';// Adjust the import to match your project structure
+import 'package:cloud_firestore/cloud_firestore.dart';
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
-void main() {
+  // Initialize Firebase
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: "AIzaSyALJQG8b9mGUU-2aOSSXYred03q2550R0Q",
+      appId: "1:637344477928:android:90d7e3d782d0c7eaa24f5d",
+      messagingSenderId: "637344477928",
+      projectId: "meeting-ds",
+    ),
+  );
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+
   runApp(const MyApp());
 }
 
@@ -12,6 +24,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: Splash());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Splash(), // Update with your actual home widget
+    );
   }
 }
