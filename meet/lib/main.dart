75c0f7bcf8239503a83a5907eff93284c1774f26
Devisree <143163502+devi1262005@users.noreply.github.com>
2024-09-10 15:28:42 +0530

    Update main.dart

--- meet/lib/main.dart
+++ meet/lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'page/Splash.dart';// Adjust the import to match your project structure
+import 'page/Splash.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Initialize Firebase
+  
   await Firebase.initializeApp(
     options: const FirebaseOptions(
       apiKey: "AIzaSyALJQG8b9mGUU-2aOSSXYred03q2550R0Q",
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Splash(), // Update with your actual home widget
+      home: Splash(), 
     );
   }
 }
