b0baf1267a2e688b770170baf3db35b5d54664a2
sooriyaravi <125038762+sooriyaravi@users.noreply.github.com>
2024-09-11 10:25:42 +0530

    Update Splash.dart

--- meet/lib/page/Splash.dart
+++ meet/lib/page/Splash.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:meet/page/Home.dart';
-import 'package:meet/page/login.dart';
 
 class Splash extends StatefulWidget {
   const Splash({super.key});
@@ -19,22 +17,25 @@ class _Splash extends State<Splash> {
   }
 
   navigatetohome() async {
-    await Future.delayed(Duration(milliseconds: 1500), () {});
+    await Future.delayed(Duration(milliseconds: 10500), () {});
     Navigator.pushReplacement(
-        context, MaterialPageRoute(builder: (context) => LoginPage()));
+        context, MaterialPageRoute(builder: (context) => Home()));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.purple.shade800, // Set the background color to purple shade800
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image.asset(
-              'assets/img/logo.png', // Path to your logo image
-              width: 150, // Adjust the width as needed
-              height: 150, // Adjust the height as needed
+            Text(
+              'Cozy Meet',
+              style: GoogleFonts.museoModerno( // Example font style, you can change it
+                fontSize: 40,
+                color: Colors.white, // Text color white for better contrast
+              ),
             ),
           ],
         ),
