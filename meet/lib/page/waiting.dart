9116dc0cb55c7cdca5b26a68e9556a7075e53d9f
sooriyaravi <125038762+sooriyaravi@users.noreply.github.com>
2024-09-11 20:34:44 +0530

    Update waiting.dart

--- meet/lib/page/waiting.dart
+++ meet/lib/page/waiting.dart
@@ -6,7 +6,7 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:meet/page/videocall.dart';
 
-class   Waiting extends StatefulWidget {
+class Waiting extends StatefulWidget {
   const Waiting({super.key});
 
   @override
@@ -30,107 +30,104 @@ class _CreateState extends State<Waiting> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 4,
-        backgroundColor: const Color.fromARGB(255, 255, 255, 255),
-        shadowColor: Colors.black.withOpacity(0.1),
-        title: Text(
-          'Waiting Room',
-          style: GoogleFonts.poppins(
-            fontSize: 24,
-            color: Colors.purple.shade800,
-            fontWeight: FontWeight.normal,
+        appBar: AppBar(
+          elevation: 4,
+          backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+          shadowColor: Colors.black.withOpacity(0.1),
+          title: Text(
+            'Waiting Room',
+            style: GoogleFonts.poppins(
+              fontSize: 24,
+              color: Colors.purple.shade800,
+              fontWeight: FontWeight.normal,
+            ),
+          ),
+          leading: IconButton(
+            icon: FaIcon(
+              FontAwesomeIcons.arrowLeft,
+            ),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+          bottom: PreferredSize(
+            preferredSize: const Size.fromHeight(2.0),
+            child: Container(
+              color: Colors.black.withOpacity(0.2),
+              height: 2.0,
+            ),
           ),
         ),
-        leading: IconButton(
-          icon: FaIcon(
-            FontAwesomeIcons.angleLeft,
-            color: Colors.purple.shade800,
-          ),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
-        bottom: PreferredSize(
-          preferredSize: const Size.fromHeight(2.0),
-          child: Container(
-            color: Colors.black.withOpacity(0.2),
-            height: 2.0,
-          ),
-        ),
-      ),
-     body:Container(
-       // Light purple background color
-      child:  Align(
-        alignment: Alignment.topCenter,
-        child: Padding(
-          padding: const EdgeInsets.only(top: 40.0), // Adjusts top padding
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              const SizedBox(height: 20),
-              Text(
-                'Welcome!\nReady to join',
-                textAlign: TextAlign.center,
-                style: GoogleFonts.poppins(
-                  fontSize: 28,
-                  color: Colors.purple.shade800,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-             
-              const SizedBox(height: 20),
-              CircleAvatar(
-                radius: 50,
-                backgroundColor: Colors.purple.shade800,
-                child: FaIcon(
-                  FontAwesomeIcons.user,
-                  color: Colors.white,
-                  size: 50, // Adjust the size of the icon
-                ),
-              ),
-              const SizedBox(height: 20),
-              Text(
-                'Host is in the call',
-                style: GoogleFonts.poppins(
-                  fontSize: 18,
-                  color: Colors.black87,
-                  fontWeight: FontWeight.normal,
-                ),
-              ),
-              
-              const SizedBox(height: 20),
-              OutlinedButton(
-                onPressed: () {
-                  // Add your onPressed functionality here
-                   Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => VideoCallPage()));
-                },
-                child: Text(
-                  'Join meeting',
-                  style: GoogleFonts.poppins(
-                    fontSize: 16,
-                    color: Colors.purple.shade800,
-                    fontWeight: FontWeight.bold,
+        body: Container(
+          // Light purple background color
+          child: Align(
+            alignment: Alignment.topCenter,
+            child: Padding(
+              padding: const EdgeInsets.only(top: 40.0), // Adjusts top padding
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const SizedBox(height: 20),
+                  Text(
+                    'Welcome!\nReady to join',
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.poppins(
+                      fontSize: 28,
+                      color: Colors.purple.shade800,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
+                  const SizedBox(height: 20),
+                  CircleAvatar(
+                    radius: 50,
+                    backgroundColor: Colors.purple.shade800,
+                    child: FaIcon(
+                      FontAwesomeIcons.user,
+                      color: Colors.white,
+                      size: 50, // Adjust the size of the icon
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Text(
+                    'Host is in the call',
+                    style: GoogleFonts.poppins(
+                      fontSize: 18,
+                      color: Colors.black87,
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  OutlinedButton(
+                    onPressed: () {
+                      // Add your onPressed functionality here
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => VideoCallPage()));
+                    },
+                    child: Text(
+                      'Join meeting',
+                      style: GoogleFonts.poppins(
+                        fontSize: 16,
+                        color: Colors.purple.shade800,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Text(
+                    'Join and Use phone for audio!',
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.poppins(
+                      fontSize: 20,
+                      color: Colors.purple.shade800,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
               ),
-              const SizedBox(height: 20),
-              Text(
-                'Join and Use phone for audio!',
-                textAlign: TextAlign.center,
-                style: GoogleFonts.poppins(
-                  fontSize: 20,
-                  color: Colors.purple.shade800,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ],
+            ),
           ),
-        ),
-      ),
-      )
-      
-    );
+        ));
   }
 }
